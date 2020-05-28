d4e955b994fe3a441a1bd42a870a10715c2a6ee8
Tony Pan <tonypan2000@gmail.com>
2020-05-28 13:56:37 -0400

    use Datastore to store comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,43 +17,66 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
-import com.google.sps.data.ServerStats;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Servlet that returns some example content. This file handles comment data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  
+  private class Comment {
+    String name;
+    String timestamp;
+    String content;
 
-  // initialize user input comments to a new ArrayList
-  // <name, comments>
-  // maps all comments to each username
-  private HashMap<String, ArrayList<String>> comments;
-
-  @Override
-  public void init() {
-    comments = new HashMap<String, ArrayList<String>>();  
+    private Comment(String nameInput, String timestampInput, String contentInput) {
+      name = nameInput;
+      timestamp = timestampInput;
+      content = contentInput;
+    }
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<Comment> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+        String name = (String) entity.getProperty("name");
+        String timestamp = (String) entity.getProperty("timestamp");
+        String content = (String) entity.getProperty("content");
+
+        Comment comment = new Comment(name, timestamp, content);
+        comments.add(comment);
+    }
+
+    Gson gson = new Gson();
+
     // Send the JSON as the response
     response.setContentType("application/json;");
-    String commentsInJson = convertToJsonWithGson(comments);
-    response.getWriter().println(commentsInJson);
+    response.getWriter().println(gson.toJson(comments));
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the name and comment from the form and add it to comments
+    // Get the name and comment from the post request
     String name = getParam(request, "name-input", "");
     String comment = getParam(request, "comment-input", "");
+
     // TODO: a more elegant way of error checking/notifying user 
     if (name.isEmpty()) {
       response.setContentType("text/html");
@@ -64,34 +87,22 @@ public class DataServlet extends HttpServlet {
       response.getWriter().println("Please enter a valid comment.");
       return;
     } 
-    if (comments.get(name) == null) {
-      ArrayList<String> commentsFromName = new ArrayList<String>();
-      commentsFromName.add(comment);
-      comments.put(name, commentsFromName);
-    } else {
-      comments.get(name).add(comment);
-    }
+
+    // get the current timestamp
+    LocalDateTime currentDate = LocalDateTime.now();
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+    String timestamp = currentDate.format(dateFormatter);
+
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("name", name);
+    commentEntity.setProperty("timestamp", timestamp);
+    commentEntity.setProperty("content", comment);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
     
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
-  }
-
-  /**
-   * Converts an ArrayList of Strings into a JSON String using the Gson library
-   */
-  private String convertToJsonWithGson(ArrayList<String> input) {
-    Gson gson = new Gson();
-    String json = gson.toJson(input);
-    return json;
-  }
-
-  /**
-   * Converts an ArrayList of Strings into a JSON String using the Gson library
-   */
-  private String convertToJsonWithGson(HashMap<String, ArrayList<String>> input) {
-    Gson gson = new Gson();
-    String json = gson.toJson(input);
-    return json;
   }
 
   /**
